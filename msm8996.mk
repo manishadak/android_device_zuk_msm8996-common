340535410fc6e3220ec6b3c6e79966b36d20d26d
Danish1999 <danish01031999@gmail.com>
2018-05-27 06:55:45 +0000

    Cardinal Bringup

--- msm8996.mk
+++ msm8996.mk
@@ -30,7 +30,7 @@ $(call inherit-product, $(LOCAL_PATH)/hidl.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-cardinal
 
 # Permissions
 PRODUCT_COPY_FILES += \
