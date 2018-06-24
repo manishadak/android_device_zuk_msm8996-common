e21cc82188391cd61df40546b1c6ad514ae90eaa
Danish1999 <danish01031999@gmail.com>
2018-06-25 00:43:46 +0530

    Revert "Resurruct hardware button"
    This reverts commit 8bff043843717bb3c58749ce55776379ea955aef.

--- msm8996.mk
+++ msm8996.mk
@@ -30,6 +30,7 @@ $(call inherit-product, $(LOCAL_PATH)/hidl.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
